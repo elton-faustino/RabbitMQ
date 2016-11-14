4d80a9f1fc73af92cd7576083e548d4920222ce3
elton-faustino <elton-faustino@hotmail.com>
2016-11-14 17:03:38 -0200

    Add files via upload

---
+++ RabbitMQ/Example1/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connect();
+        }
+
+        IConnection _connection;
+
+        public void Connect()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "test",
+                Password = "test",
+                RequestedHeartbeat = 60
+            };
+
+            _connection = connectionFactory.CreateConnection();
+        }
+    }
+}
